6862402aa45d57aa69b7f7b993d769f0190f229d
npkyphuong04 <kyphuong0414@gmail.com>
2023-12-04 21:43:31 +0700

    2nd commit

--- DataVisualization/urls.py
+++ DataVisualization/urls.py
@@ -15,8 +15,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from draft import views
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-]
+                  path('', views.draft, name='draft'),
+                  path('mysecond', views.mysecond, name='mysecond'),
+                  path('', include('draft.urls')),
+                  path('admin/', admin.site.urls),
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
