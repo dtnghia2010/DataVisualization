7295ea440b2d425856d57d18f9547aadd3cfe130
nathang0147 <nganhthang147@gmail.com>
2023-12-07 22:05:48 +0700

    hello

--- draft/views.py
+++ draft/views.py
@@ -8,56 +8,61 @@ from collections import Counter
 
 def home_view(request, *args, **kwargs):
     # global attributeid1, attributeid2
-    context={}
-    if request.method == 'POST':
-        uploaded_file = request.FILES['document']
+    context = {}
+    if request.method == "POST":
+        uploaded_file = request.FILES["document"]
         # attributeid1 = request.POST.get('attributeid1')
         # attributeid2 = request.POST.get('attributeid2')
 
         # print(uploaded_file)
-        if uploaded_file.name.endswith('csv'):
+        if uploaded_file.name.endswith("csv"):
             # save the file in media folder
             savefile = FileSystemStorage()
             name = savefile.save(uploaded_file.name, uploaded_file)
-            #know where to save the file
-            d = os.getcwd() #get the current directory
-            file_directory = d+'/media/'+name
+            # know where to save the file
+            d = os.getcwd()  # get the current directory
+            file_directory = d + "/media/" + name
             # getOptions(request)
             readfile(file_directory)
             return redirect(options)
         else:
-            messages.warning(request, 'File was not uploaded, please use csv file extension')
+            messages.warning(
+                request, "File was not uploaded, please use csv file extension"
+            )
     return render(request, "draft/myfirst.html", {})
+
 
 def options(request):
     global attributeid1, attributeid2
-    if request.method == 'POST':
-        attributeid1 = request.POST.get('attributeid1')
-        attributeid2 = request.POST.get('attributeid2')
+    if request.method == "POST":
+        attributeid1 = request.POST.get("attributeid1")
+        attributeid2 = request.POST.get("attributeid2")
         print(attributeid1)
         print(attributeid2)
         return redirect(results)
     return render(request, "draft/options.html", {})
 
+
 def readfile(filename):
     global data
-    my_file = pd.read_csv(filename, sep='[:;,|_]', engine='python')
-    data = pd.DataFrame(data = my_file, index=None)
+    my_file = pd.read_csv(filename, sep="[:;,|_]", engine="python")
+    data = pd.DataFrame(data=my_file, index=None)
     print(data)
 
+
 def results(request):
-    #split into keys and values based on the attribute input
+    # split into keys and values based on the attribute input
     labels = []
     datas = []
-    for x in data[attributeid1]: #name
+    for x in data[attributeid1]:  # name
         labels.append(x)
 
-    for y in data[attributeid2]: #price
+    for y in data[attributeid2]:  # price
         datas.append(y)
 
     my_labels = dict(Counter(labels))
     my_datas = dict(Counter(datas))
-    print('my dashboard ', my_labels)
+    print("my dashboard ", my_labels)
     labels = my_labels.keys()
     datas = my_datas.keys()
     # values = my_dashboard.values()
@@ -78,12 +83,14 @@ def results(request):
     #     listvalues.append(y)
 
     context = {
-        'listlabels':listlabels,
-        'listdatas':listdatas,
+        "listlabels": listlabels,
+        "listdatas": listdatas,
         # 'listvalues':listvalues,
     }
 
     return render(request, "draft/results.html", context)
+
+
 ####
 # def draft(request):
 #     context = {}
@@ -111,22 +118,3 @@ def results(request):
 #
 #
 #
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
