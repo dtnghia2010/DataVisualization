5e15f69bd80748106f9d26ab4b64b24df2a8d304
thuongngo050902 <ngothithuong592002@gmail.com>
2023-12-07 08:53:30 +0700

    Initial commit

---
+++ dashboard/forms.py
@@ -0,0 +1,7 @@
+from django import forms
+from .models import CountryData
+
+class CountryDataFrom(forms.ModelForm):
+    class Meta:
+        model = CountryData
+        fields='__all__'
---
+++ dashboard/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class CountryData(models.Model):
+    country = models.CharField(max_length=100)
+    population = models.IntegerField()
+
+    class Meta:
+     verbose_name_plural='Country Population Data'
+    def __str__(self):
+        return f'{self.country}-{self.population}'
+
