358d3cad2cd10428e3076bccaf50b817ca2e271f
nathang0147 <nganhthang147@gmail.com>
2023-12-09 12:48:02 +0700

    hello

---
+++ dashboard/util.py
@@ -0,0 +1,9 @@
+import csv
+
+def read_csv(file_path):
+    data = []
+    with open(file_path, 'r') as file:
+        csv_reader = csv.reader(file)
+        for row in csv_reader:
+            data.append(row)
+    return data
