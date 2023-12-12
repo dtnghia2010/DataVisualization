e10e9ed889f351336eacd31fe41dc865e86d89c3
thuongngo050902 <ngothithuong592002@gmail.com>
2023-12-12 16:26:17 +0700

    csv to database

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,7 +1,10 @@
+# models.py
+
 from django.db import models
 
 
 class Add_Data(models.Model):
+    # Mô hình cho dữ liệu về quốc gia và dân số
     country = models.CharField(max_length=100)
     population = models.IntegerField()
 
@@ -12,14 +15,10 @@ class Add_Data(models.Model):
         return f'{self.country}-{self.population}'
 
 
-
-
-
-
 class Upload_File(models.Model):
+    # Mô hình cho dữ liệu từ tệp CSV được tải lên
     attribute1 = models.CharField(max_length=100, null=True, default=None)
     attribute2 = models.BigIntegerField(null=True, default=None)
-def __str__(self):
-        return f'{self.attribute1}-{self.atrribute2}'
 
-
+    def __str__(self):
+        return f'{self.attribute1}-{self.attribute2}'
