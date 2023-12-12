2a2903df3e04e986ce715cf2eaf7538e1acde219
thuongngo050902 <ngothithuong592002@gmail.com>
2023-12-12 16:29:21 +0700

    csv to database

--- dashboard/forms.py
+++ dashboard/forms.py
@@ -1,15 +1,18 @@
+# forms.py
+
 from django import forms
 from .models import Add_Data, Upload_File
 
 
 class Add_DataFrom(forms.ModelForm):
+    # Biểu mẫu cho dữ liệu về quốc gia và dân số
     class Meta:
         model = Add_Data
         fields = '__all__'
 
 
-
 class Upload_FileForm(forms.ModelForm):
+    # Biểu mẫu cho dữ liệu từ tệp CSV được tải lên
     class Meta:
         model = Upload_File
         fields = ['attribute1', 'attribute2']
