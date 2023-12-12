2fe391e9546a3ab2ec7488c9db6c75fb437c2818
thuongngo050902 <ngothithuong592002@gmail.com>
2023-12-12 16:30:35 +0700

    csv to database

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,28 +1,24 @@
-import csv
-import io
+# views.py
+
 import os
+from collections import Counter
 
 import pandas as pd
-from django.contrib.admin.templatetags.admin_list import results
-from django.core.checks import messages
 from django.shortcuts import render, redirect
-from django.template import context
-from tablib import Dataset
+from django.core.files.storage import FileSystemStorage
+from django.contrib import messages
+from django.conf import settings
 from .models import Add_Data, Upload_File
 from .forms import Add_DataFrom, Upload_FileForm
-import csv
-import io
-from django.shortcuts import render, redirect
-from django.contrib import messages
 
-
-# Create your views here.
 
 def index(request):
+    # Hàm view cho trang chủ
     return render(request, 'dashboard/index.html')
 
 
 def add_data(request):
+    # Hàm view cho việc thêm dữ liệu quốc gia và dân số
     data = Add_Data.objects.all()
 
     if request.method == 'POST':
@@ -52,19 +48,8 @@ def add_data(request):
     return render(request, 'dashboard/add_data.html', context)
 
 
-
-import os
-from collections import Counter
-
-from django.shortcuts import render, redirect
-from django.core.files.storage import FileSystemStorage
-from django.contrib import messages
-import pandas as pd
-
-from django.conf import settings
-from django.core.files.storage import FileSystemStorage
-
 def upload_file(request, *args, **kwargs):
+    # Hàm view cho việc tải lên tệp CSV và lưu trữ dữ liệu vào database
     global attribute1, attribute2
     context = {}
     listlabels, listdatas = None, None
@@ -96,105 +81,17 @@ def upload_file(request, *args, **kwargs):
 
     return render(request, "dashboard/upload_file.html", {'listlabels': listlabels, 'listdatas': listdatas})
 
-# def upload_file(request, *args, **kwargs):
-#     global attribute1, attribute2
-#     context = {}
-#     listlabels, listdatas = None, None
-#
-#     if request.method == 'POST':
-#         uploaded_file = request.FILES['document']
-#         attribute1 = request.POST.get('attribute1')
-#         attribute2 = request.POST.get('attribute2')
-#
-#         if uploaded_file.name.endswith('csv'):
-#             savefile = FileSystemStorage()
-#             name = savefile.save(uploaded_file.name, uploaded_file)
-#
-#             file_directory = os.path.join(settings.MEDIA_ROOT, name)
-#             readfile(file_directory)
-#
-#             labels, datas = process_data(attribute1, attribute2)
-#             listlabels, listdatas = prepare_chart_data(labels, datas)
-#
-#         else:
-#             messages.warning(request, 'File was not uploaded, please use a CSV file extension')
-#
-#     return render(request, "dashboard/upload_file.html", {'listlabels': listlabels, 'listdatas': listdatas})
-
 
 def readfile(filename):
+    # Hàm đọc dữ liệu từ tệp CSV và lưu vào biến toàn cục `data`
     global data
     my_file = pd.read_csv(filename, sep='[:;,|_]', engine='python')
     data = pd.DataFrame(data=my_file, index=None)
-    print(data)
 
 
 def process_data(attribute1, attribute2):
+    # Hàm xử lý dữ liệu từ DataFrame và trả về danh sách nhãn và dữ liệu
     labels = []
     datas = []
     for x in data[attribute1]:  # name
         labels.append(x)
-
-    for y in data[attribute2]:  # price
-        datas.append(y)
-
-    return labels, datas
-
-
-def prepare_chart_data(labels, datas):
-    my_labels = dict(Counter(labels))
-    my_datas = dict(Counter(datas))
-
-    listlabels = list(my_labels.keys())
-    listdatas = list(my_datas.keys())
-
-    return listlabels, listdatas
-
-
-
-    #     form = UploadFileForm(request.POST, request.FILES)
-    #     if form.is_valid():
-    #         uploaded_file = form.save(commit=False)
-    #
-    #         # Đọc dữ liệu từ file CSV
-    #         csv_data = read_csv(uploaded_file.file.path)
-    #
-    #         # Lưu trữ dữ liệu vào cơ sở dữ liệu
-    #         for row in csv_data:
-    #             # Tạo một bản ghi mới cho mỗi dòng trong CSV
-    #             new_record = CountryData(attribute1=uploaded_file.attribute1, attribute2=uploaded_file.attribute2, country=row[0], population=row[1])
-    #             new_record.save()
-    #
-    #         uploaded_file.save()
-    #
-    #         return redirect('upload_file')
-    # else:
-    #     form = UploadFileForm()
-    #
-    # uploaded_files = UploadedFile.objects.all()
-
-    # return render(request, 'dashboard/upload_file.html')
-
-# from django.shortcuts import render, redirect
-# from .models import UploadedFile
-# from .forms import UploadFileForm
-#
-# def upload_file(request):
-#     if request.method == 'POST':
-#         form = UploadFileForm(request.POST, request.FILES)
-#         if form.is_valid():
-#             uploaded_file = form.save(commit=False)
-#             uploaded_file.save()
-#
-#             # Cập nhật biểu đồ ở đây (sử dụng uploaded_file)
-#
-#             return redirect('upload_file')  # Hoặc chuyển hướng đến trang khác nếu cần
-#     else:
-#         form = UploadFileForm()
-#
-#     uploaded_files = UploadedFile.objects.all()
-#
-#     return render(request, 'dashboard/upload_file.html', {'form': form, 'uploaded_files': uploaded_files})
-#
-
-# your_app/views.py
