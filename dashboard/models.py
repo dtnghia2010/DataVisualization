7c312ee88275abf3271da5b68bfcf17443532089
thuongngo050902 <ngothithuong592002@gmail.com>
2023-12-12 16:18:09 +0700

    csv to database

--- dashboard/models.py
+++ dashboard/models.py
@@ -12,18 +12,9 @@ class Add_Data(models.Model):
         return f'{self.country}-{self.population}'
 
 
-# models.py
-# models.py
-from django.db import models
 
-# models.py
-from django.db import models
 
-# models.py
-from django.db import models
 
-# models.py
-from django.db import models
 
 class Upload_File(models.Model):
     attribute1 = models.CharField(max_length=100, null=True, default=None)
