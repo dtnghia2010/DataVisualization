37b6f06177f8a878db0f428188c2f696cf07c452
npkyphuong04 <kyphuong0414@gmail.com>
2023-12-14 21:29:57 +0700

    commit

--- datavisual/settings.py
+++ datavisual/settings.py
@@ -120,13 +120,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
-MEDIA_ROOT = BASE_DIR / 'media'
-MEDIA_URL = '/media/'
+MEDIA_URL = '/images/'
 
 
 # Default primary key field type
