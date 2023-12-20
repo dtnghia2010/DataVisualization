9123b9331fb12ef60c711ac94fb8f30415f821f4
dtnghia2010 <ititiu21256@student.hcmiu.edu.vn>
2023-12-20 16:29:24 +0700

    csv to database

--- algorithm/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class AlgorithmConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "algorithm"
--- algorithm/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from . import views
-
-app_name = 'algorithm'
-urlpatterns = [
-    path('', views.index, name = 'index'),
-]
