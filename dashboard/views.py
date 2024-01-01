3d917592272c67b94dff7d7b2995ea866f02762f
thuongngo050902 <ngothithuong592002@gmail.com>
2024-01-01 18:52:34 +0700

    FIX BUG LOGIC _ FINAL BUG

--- dashboard/views.py
+++ dashboard/views.py
@@ -302,14 +302,12 @@ def search_page_upload(request):
                           {'listlabels': labels, 'listdatas': datas, 'selected_data': selected_data,
                            'error_message': error_message, 'value_a': value_a, 'value_b': value_b})
 
-
-        except (ValueError, TypeError):
-
-            return HttpResponse("Invalid input values. Please enter valid integers for 'a' and 'b'.")
+        except (ValueError, TypeError) as e:
+            # Xử lý giá trị nhập không hợp lệ
+            return HttpResponse(f"Lỗi: {e}")
 
     # Render form khi request là GET
     return render(request, 'dashboard/search_page_upload.html', {'error_message': error_message})
-
 
 def search_add_data(request):
     if request.method == 'POST':
